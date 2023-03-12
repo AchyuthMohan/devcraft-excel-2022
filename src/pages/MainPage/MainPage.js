e64f7ae77b2e158ae64035ed0f938c3dda751799
jacobsanosh <jacobsanosh@gmail.com>
2023-03-12 11:29:23 +0530

    changes

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import React, { useContext } from "react";
-import { UserContext } from "../../contexts/UserContext";
-import MainLayout from "../../components/MainLayout/MainLayout";
-import "./MainPage.css";
-=======
 import React from 'react'
 import MainLayout from '../../components/MainLayout/MainLayout'
 import{Navbar} from '../../components/Navbar/Navbar'
@@ -11,35 +5,11 @@ import{About} from '../../components/About/About'
 import {MoreDetails} from '../../components/MoreDetails/MoreDetails'
 import './MainPage.css'
 import Hero from '../../components/Hero/Hero'
->>>>>>> 0318d43 (component edition)
 const MainPage = () => {
   const { signedInUser, signInWithGoogle, signOutOfGoogle } =
     useContext(UserContext);
   return (
     <MainLayout>
-<<<<<<< HEAD
-      <h1>Main Page</h1>
-      <button
-        onClick={() => {
-          if (signedInUser) {
-            signOutOfGoogle();
-          } else {
-            signInWithGoogle();
-          }
-        }}
-      >
-        {signedInUser ? "Sign Out" : "Sign In"}
-      </button>
-      {signedInUser && (
-        <div>
-          <h1>{signedInUser.displayName}</h1>
-          <img src={signedInUser.photoURL} alt="" />
-        </div>
-      )}
-    </MainLayout>
-  );
-};
-=======
       <Navbar/>
       <Hero/>
       <About/>
@@ -47,6 +17,5 @@ const MainPage = () => {
     </MainLayout>
   )
 }
->>>>>>> 0318d43 (component edition)
 
 export default MainPage;
