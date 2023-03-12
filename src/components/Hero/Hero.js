664f4ea18731abfad2a8a77abdaac8eaa3e6fd40
jaison080 <jaisondennis080@gmail.com>
2023-03-12 13:35:49 +0530

    Integarted Login

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Hero.css";
 import hero from "../../assets/Landing/hero.svg";
+import { UserContext } from "../../contexts/UserContext";
+import { useNavigate } from "react-router-dom";
 
 function Hero() {
+  const { signedInUser, signInWithGoogle, signOutOfGoogle, setSignedInUser } =
+    React.useContext(UserContext);
+  const navigate = useNavigate();
   return (
     <>
       <div className="hero">
@@ -14,7 +19,21 @@ function Hero() {
             stay motivated towards a healthier you. Start crushing your goals
             one meal at a time!
           </div>
-          <div className="hero__container__text__button">Get Started</div>
+          <div
+            className="hero__container__text__button"
+            style={{
+              cursor: "pointer",
+            }}
+            onClick={() => {
+              if (signedInUser) {
+                navigate("/diet");
+              } else {
+                signInWithGoogle();
+              }
+            }}
+          >
+            {signedInUser ? "View Dashboard" : " Get Started"}
+          </div>
         </div>
         <div className="hero__container__image">
           <img src={hero} alt="hero" />
