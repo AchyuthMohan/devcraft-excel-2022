adb44fd5100ea7b52a99307717778aa03a973c60
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-03-12 11:11:12 +0530

    css

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
+    
   return (
     <div className='nav__main'>
-        <span className='nav__section'>Home</span>
-        <span className='nav__section'>Diet Plans</span>
-        <span className='nav__section'>Get Plan</span>
+        <Link to='/' className='nav__section'>Home</Link>
+        <Link to='/plans' className='nav__section'>Diet Plans</Link>
+        <Link className='nav__section'>Get Plan</Link>
     </div>
   )
 }
