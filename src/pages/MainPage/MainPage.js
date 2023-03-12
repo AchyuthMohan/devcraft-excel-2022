d771f04e1d62612fa8e270a7dff565c0baaa50a6
jacobsanosh <jacobsanosh@gmail.com>
2023-03-12 11:27:18 +0530

    fixed

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import MainLayout from '../../components/MainLayout/MainLayout'
-import{Navbar} from '../../components/Navbar/Navbar'
-import{About} from '../../components/About/About'
-import {MoreDetails} from '../../components/MoreDetails/MoreDetails'
+import Navbar from '../../components/Navbar/Navbar'
+import About from '../../components/About/About'
+import MoreDetails from '../../components/MoreDetails/MoreDetails'
 import './MainPage.css'
 import Hero from '../../components/Hero/Hero'
 const MainPage = () => {
