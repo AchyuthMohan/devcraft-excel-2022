f0126acb2a56f0c7f45420b3739eb7fc8e704c3f
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-03-12 10:54:09 +0530

    changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import './Navbar.css'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='nav__main'>
+        <span className='nav__section'>Home</span>
+        <span className='nav__section'>Diet Plans</span>
+        <span className='nav__section'>Get Plan</span>
+    </div>
   )
 }
 
